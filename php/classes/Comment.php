1313baa2b6f7bfb27558c588a5272270f6442d16
Daniel <danieleaton747@gmail.com>
2016-10-20 21:48:25 -0600

    Redid Data design, took out unnecessary inheritance and added DDL diagram I forgot to put on

--- php/classes/Comment.php
+++ php/classes/Comment.php
@@ -9,22 +9,38 @@
 //Includes the Profile.php file
 require 'Profile.php';
 
-class Comment extends Profile{
+class Comment extends Profile {
 
 	/**
 	 * @var int commentId The Comment Identification number used to identify it in the database
-	 * @var int commentParentId The Comment Identification number this comment is replying to
-	 * @var int commentProfileId The Profile Identification number of the User who made the comment
-	 * @var int commentVoteScore The Score of the comment measured by upvotes subtracted by downvotes
-	 * @var datetime commentTime The Time the comment was made
-	 * @var string commentText The text and actual content of the comment itself
 	 */
-	private $commentId;
-	private $commentParentId;
-	private $commentProfileId;
-	private $commentVoteScore;
-	private $commentTime;
-	private $commentText;
+		private $commentId;
+	/**
+	 * @var int commentParentId The Comment Identification number this comment is replying to
+	 */
+		private $commentParentId;
+	/**
+	 * @var int commentProfileId The Profile Identification number of the User who made the comment
+	 */
+		private $commentProfileId;
+	/**
+	 * @var int commentVoteScore The Score of the comment measured by upvotes subtracted by downvotes
+	 */
+		private $commentVoteScore;
+
+	/**
+	 * @var datetime commentTime The Time the comment was made
+	 */
+		private $commentTime;
+
+	/**
+	 * @var string commentText The text and actual content of the comment itself
+	 *
+	 */
+		private $commentText;
+
+
+
 	private $profile;
 
 	/**
@@ -35,9 +51,8 @@ class Comment extends Profile{
 	 * @param $commentVoteScore int the parameter that will be passed into commentProfileId
 	 * @param $commentTime int the parameter that will be passed into commentTime
 	 * @param $commentText string the parameter that will be passed into commentText
-	 * @param $profile Profile a profile of the user who made that comment
 	 */
-	public function __construct($commentId,$commentParentId,$commentProfileId, $commentVoteScore,$commentTime,$commentText , Profile $profile) {
+	public function __construct($commentId, $commentParentId, $commentProfileId, $commentVoteScore, $commentTime, $commentText) {
 		$this->commentId = $this->setCommentId($commentId);
 		$this->commentParentId = $this->setCommentParentId($commentParentId);
 		$this->commentProfileId = $this->setCommentProfileId($commentProfileId);
@@ -53,7 +68,7 @@ class Comment extends Profile{
 	 *
 	 * @return int returns the comment identification number
 	 */
-	public function getCommentId(){
+	public function getCommentId() {
 		return $this->commentId;
 	}
 
@@ -63,15 +78,13 @@ class Comment extends Profile{
 	 * @param $commentIdInput int Input that will be sanitized and passed through to the commentId property
 	 * @throws Exception 'This number is not an integer' when user tries to enter in a value other than an int
 	 */
-	private function setCommentId($commentIdInput){
-		if(!is_int($commentIdInput)){
+	private function setCommentId($commentIdInput) {
+		if(!is_int($commentIdInput)) {
 			throw new Exception("This number is not an Integer");
 		}
 		try {
 			$this->commentId = filter_var($commentIdInput, FILTER_SANITIZE_NUMBER_INT);
-		}
-		catch(Exception $e)
-		{
+		} catch(Exception $e) {
 			$e->getMessage();
 		}
 	}
@@ -80,7 +93,7 @@ class Comment extends Profile{
 	 * Returns value of commentParentId
 	 * @return int returns the commentParent identification) number
 	 */
-	public function getCommentParentId(){
+	public function getCommentParentId() {
 		return $this->commentParentId;
 	}
 
@@ -89,14 +102,12 @@ class Comment extends Profile{
 	 * @param $commentParentInput int Theinput that will be sanitized and passed to commentParentId
 	 * @throws Exception "This Value is Not an Integer" when input value is not an integer
 	 */
-	private function setCommentParentId($commentParentInput){
+	private function setCommentParentId($commentParentInput) {
 		if(!is_int($commentParentInput))
 			throw new Exception("This value is not an Integer");
 		try {
 			$this->commentParentId = filter_var($commentParentInput, FILTER_SANITIZE_NUMBER_INT);
-		}
-		catch(Exception $e)
-		{
+		} catch(Exception $e) {
 			$e->getMessage();
 		}
 
@@ -108,7 +119,7 @@ class Comment extends Profile{
 	 * Returns the value of commentProfileId
 	 * @return int returns the comment profile identification number
 	 */
-	public function getCommentProfileId(){
+	public function getCommentProfileId() {
 		return $this->commentProfileId;
 	}
 
@@ -117,13 +128,13 @@ class Comment extends Profile{
 	 * @param $userInputId int the number that will be sanitized and passed through to the commentProfileId
 	 * @throws Exception If user has antered a value that is not an integer
 	 */
-	private function setCommentProfileId($userInputId){
-		if(!is_int($userInputId)){
+	private function setCommentProfileId($userInputId) {
+		if(!is_int($userInputId)) {
 			throw new Exception("The value entered is not an integer");
 		}
-		try{
+		try {
 			$this->commentProfileId = filter_var($userInputId, FILTER_SANITIZE_NUMBER_INT);
-		}catch(Exception $e){
+		} catch(Exception $e) {
 			$e->getMessage();
 		}
 	}
@@ -133,7 +144,7 @@ class Comment extends Profile{
 	 *
 	 * @return int returns the comment vote score
 	 */
-	public function getCommentVoteScore(){
+	public function getCommentVoteScore() {
 		return $this->commentVoteScore;
 	}
 
@@ -142,14 +153,14 @@ class Comment extends Profile{
 	 * @param $userVote int The input that will be passed into the commentVoteScore property. It will also be sanitized before it is passed.
 	 * @throws Exception if $userVote is set to a value other than int
 	 */
-	public function setCommentVoteScore($userVote){
-		if(!is_int($userVote)){
+	public function setCommentVoteScore($userVote) {
+		if(!is_int($userVote)) {
 			throw new Exception("The value entered is not an integer");
 		}
-		try{
-		$userVote = filter_var($userVote, FILTER_SANITIZE_NUMBER_INT);
-		$this->commentVoteScore = $userVote;}
-		catch(Exception $e){
+		try {
+			$userVote = filter_var($userVote, FILTER_SANITIZE_NUMBER_INT);
+			$this->commentVoteScore = $userVote;
+		} catch(Exception $e) {
 			$e->getMessage();
 		}
 	}
@@ -158,7 +169,7 @@ class Comment extends Profile{
 	 * Returns the value of commentTime()
 	 * @return datetime returns the comment's time stamp in milliseconds from
 	 */
-	public function getCommentTime(){
+	public function getCommentTime() {
 		return $this->commentTime;
 	}
 
@@ -167,14 +178,14 @@ class Comment extends Profile{
 	 * @param $userTimeInput
 	 * @throws Exception of $userTimeInput is set to a value that is not DateTime
 	 */
-	public function setCommentTime($userTimeInput){
+	public function setCommentTime($userTimeInput) {
 		if(!is_a($userTimeInput, 'DateTime')) {
 			throw new Exception("This value is not a DateTime");
 		}
 
-		try{
+		try {
 			$this->commentTime = $userTimeInput;
-		}catch(Exception $e) {
+		} catch(Exception $e) {
 			$e->getMessage();
 		}
 
@@ -184,7 +195,7 @@ class Comment extends Profile{
 	 * Returns the string value of commentText
 	 * @return string returns the comment Text()
 	 */
-	public function getCommentText(){
+	public function getCommentText() {
 		return $this->commentText;
 	}
 
@@ -193,40 +204,14 @@ class Comment extends Profile{
 	 * @param $textInput string The text that will be input to be validized ands anitized, then sent to the commentText property
 	 * @throws Exception if the String Length of $textInput is too long
 	 */
-	public function setCommentText($textInput){
+	public function setCommentText($textInput) {
 		//
-		if(strlen($textInput > 10000)){
+		if(strlen($textInput > 10000)) {
 			$result = filter_var(FILTER_SANITIZE_STRING);
-		}
-		else{
+		} else {
 			throw new Exception("Error: Comment is too long");
 		}
 		$this->commentText = $result;
 	}
 
-	/**
-	 * Returns Username of profile who made this comment
-	 * @return String profileUsername String the username of the profile who made this comment
-	 */
-	public function getProfileUsername(){
-		parent::getProfileUsername();
-	}
-
-	/**
-	 * Returns the value of the user who made this comment's favorite Team. Used to make the flair of the user
-	 *
-	 * @return String profileTeam the favorite team of the user who made this comment
-	 */
-	public function getProfileTeam() {
-		return parent::getProfileTeam();
-	}
-
-	/**
-	 * Returns the profile identification number of the profile who made this comment
-	 *
-	 * @return int profileId the identification number of the profile who made this comment
-	 */
-	public function getProfileId() {
-		return parent::getProfileId();
-	}
 }
